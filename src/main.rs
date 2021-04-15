0be83ec4d2b43ccdc274edb9c6bfcb5234161b8f
yanlin0124 <yanlin.li@hrtuoyu.com>
2021-04-15 15:37:43 +0800

    yanlin：完美提交

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("猜数！");
+    println!("猜数!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
     //正式游戏不会打印神秘数字
     // println!("The secret number is: {}", secret_number);
@@ -11,7 +11,7 @@ fn main() {
         println!("请猜一个数：");
 
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("读取行失败！");
+        io::stdin().read_line(&mut guess).expect("读取行失败!");
         //转换  将字符串解析成数字
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
